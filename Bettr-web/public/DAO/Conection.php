ba8ca468e6bf0b150aa16059fd91bc04e5ad4dd2
Hugo Montes <huugomontees@gmail.com>
2025-12-28 11:56:53 +0100

    feat(database): Fix connection handling in Conection class

--- Bettr-web/public/DAO/Conection.php
+++ Bettr-web/public/DAO/Conection.php
@@ -1,20 +1,26 @@
 <?php
 class Conection{
+    private $conection;
     private $dbName;
     private $host;
     private $user;
     private $password;
 
     function init($dbName, $host, $user, $password){
+        $dsn = "mysql:host=$host;dbname=$dbName;charset=utf8";
         try {
-            $conexion = new PDO();
-        } catch (SQLite3Exception $sql) {
-            print("Error en la conexión con la base de datos");
+            $this->conection = new PDO($dsn, $user, $password);
+        } catch (PDOException $sql) {
+            print("Error en la conexión con la base de datos"); 
         }
     }
 
-    function stop(){
+    public function getConection(){
+        return $this->conection;
+    }
 
+    function stop(){
+        $this->conection = null;
     }
 }
 ?>
