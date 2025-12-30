675aa0848b1e5feea14d0ff78f2fe14dd3e7d1f0
Hugo Montes <huugomontees@gmail.com>
2025-12-30 13:21:17 +0100

    feat(user): Implement User model with properties and methods for user management

---
+++ Bettr-web/app/models/User.php
@@ -0,0 +1,67 @@
+<?php
+class User{
+    private $id;
+    private $name;
+    private $username;
+    private $email;
+    private $password;
+
+    private $avatar;
+
+    public function __construct($id, $name, $username, $email, $password, $avatar){
+        $this->id = $id;
+        $this->name = $name;
+        $this->username = $username;
+        $this->email = $email;
+        $this->password = $password;
+        $this->avatar = $avatar;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setName($name){
+        $this->name = $name;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function setUsername($username){
+        $this->username = $username;
+    }
+    
+    public function getUsername(){
+        return $this->username;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function setPassword($password){
+        $this->password = $password;
+    }
+
+    public function getPassword(){
+        return $this->password;
+    }
+
+    public function getAvatar(){
+        return $this->avatar;
+    }
+
+    public function setAvatar($avatar){
+        $this->avatar = $avatar;
+    }
+
+    public function __toString(){
+        return "User [id=$this->id, name=$this->name, username=$this->username, email=$this->email, avatar=$this->avatar]";
+    }
+}
