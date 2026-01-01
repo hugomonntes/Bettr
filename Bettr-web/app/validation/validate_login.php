d73338a347d90ed57cc216b97516c6b65787fd5e
Hugo Montes <huugomontees@gmail.com>
2026-01-01 21:54:23 +0100

    fix(validation): Correct redirect path in login validation

--- Bettr-web/app/validation/validate_login.php
+++ Bettr-web/app/validation/validate_login.php
@@ -7,7 +7,8 @@
         $usersDAO = new UsersDAO($conection->getConection());
         $usersDAO->emailExists($_POST['email']); // Verifica si el usuario existe TODO CAMBIAR A EMAIL
         if ($usersDAO && password_verify($_POST['password'], $usersDAO->getPasswordByEmail($_POST['email']))) {
-            header("Location: ../public/index.php");
+            header("Location: /Bettr-web/public/index.php");
+            exit;
         } else {
             echo "Usuario o contraseña incorrectos";
         }
