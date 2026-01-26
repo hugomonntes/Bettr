95a4bcc6d37d4a9eaf8d17e1c6cba0939dd97cd3
hmontes <hmontes.dev@gmail.com>
2026-01-26 17:26:10 +0100

    feat(API_REST): start process api_rest logic

--- api_rest/src/main/webapp/Bettr/UsersManager.java
+++ api_rest/src/main/webapp/Bettr/UsersManager.java
@@ -3,6 +3,7 @@ package Bettr;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
@@ -23,17 +24,16 @@ public class UsersManager {
             Class.forName("com.mysql.cj.jdbc.Driver");
             try (Connection conn = DriverManager.getConnection(url, this.user, password)) {
                 String query = "INSERT INTO Users (name, username, email, password) VALUES (?, ?, ?, ?)";
-                PreparedStatement pstmt = conn.prepareStatement(query);
-                pstmt.setString(1, user.getName());
-                pstmt.setString(2, user.getUsername());
-                pstmt.setString(3, user.getEmail());
-                pstmt.setString(4, user.getPassword());
-                pstmt.executeUpdate();
-                pstmt.close();
+                try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+                    pstmt.setString(1, user.getName());
+                    pstmt.setString(2, user.getUsername());
+                    pstmt.setString(3, user.getEmail());
+                    pstmt.setString(4, user.getPassword());
+                    pstmt.executeUpdate();
+                }
             }
             return Response.status(Response.Status.CREATED).build();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (ClassNotFoundException | SQLException e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
         }
     }
