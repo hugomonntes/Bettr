3fd501a934ebf8167b4400fa160a0e4e9c44b2ef
hmontes <hmontes.dev@gmail.com>
2026-01-28 12:07:24 +0100

    Add initial web application structure and sample content
    - Created web.xml for web application configuration
    - Added index.jsp with a simple "Hello World!" message
    - Included pom.properties for Maven project metadata
    - Initialized inputFiles.lst for Maven compiler plugin

---
+++ api_rest/target/api_rest/Bettr/Users.java
@@ -0,0 +1,47 @@
+package Bettr;
+
+public class Users {
+    String name;
+    String username;
+    String email;
+    String password;
+
+    public Users(String name, String username, String email, String password) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
