d8ff37f80023d1784b62220ec25fe8bef0d91997
hmontes <hmontes.dev@gmail.com>
2026-01-29 13:55:26 +0100

    feat(WriteEvent): add logic from function writeEvent

--- api_rest/src/main/java/Bettr/UsersManager.java
+++ api_rest/src/main/java/Bettr/UsersManager.java
@@ -12,6 +12,7 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -25,17 +26,59 @@ public class UsersManager {
     @POST
     @Path("/add")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response addUser(Users user) { // TODO hashear password
+    public Response addUser(Users user) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             try (Connection conn = DriverManager.getConnection(url, this.user, password)) {
                 String query = "INSERT INTO usuarios (name, username, email, password_hash) VALUES (?, ?, ?, ?)";
-                
+
                 try (PreparedStatement pstmt = conn.prepareStatement(query)) {
                     pstmt.setString(1, user.getName());
                     pstmt.setString(2, user.getUsername());
                     pstmt.setString(3, user.getEmail());
                     pstmt.setString(4, user.getPassword_hash());
+                    pstmt.executeUpdate();
+                }
+            }
+            return Response.ok().build();
+        } catch (ClassNotFoundException | SQLException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
+
+    @POST
+    @Path("/editName")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response editName(Users user) { // FIXME no tener dos users con el mismo username
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection conn = DriverManager.getConnection(url, this.user, password)) {
+                String query = "UPDATE usuarios SET name=? WHERE username=?";
+
+                try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+                    pstmt.setString(1, user.getName());
+                    pstmt.setString(2, user.getUsername());
+                    pstmt.executeUpdate();
+                }
+            }
+            return Response.ok().build();
+        } catch (ClassNotFoundException | SQLException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
+
+    @POST
+    @Path("/editUserName/{username}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response editUserName(Users user, @PathParam("username") String username) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection conn = DriverManager.getConnection(url, this.user, password)) {
+                String query = "UPDATE usuarios SET username=? WHERE username=?";
+
+                try (PreparedStatement pstmt = conn.prepareStatement(query)) {
+                    pstmt.setString(1, username);
+                    pstmt.setString(2, user.getUsername());
                     pstmt.executeUpdate();
                 }
             }
