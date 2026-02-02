777bbc18d11fa04e0178939bce5912030e380747
hugomonntes <hmontes.dev@gmail.com>
2026-02-02 22:30:14 +0100

    feat(Adapter, Api_Get): add fragments views and start to copmplete all views

--- Bettr_Mobile_App/app/src/main/java/com/example/bettr/Feed.java
+++ Bettr_Mobile_App/app/src/main/java/com/example/bettr/Feed.java
@@ -9,8 +9,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.bettr.Fragments.CameraFragment;
 import com.example.bettr.Fragments.FeedFragment;
 import com.example.bettr.Fragments.HabitsFragment;
+import com.example.bettr.Fragments.ProfileFragment;
 import com.example.bettr.Fragments.SocialFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -24,7 +26,7 @@ public class Feed extends AppCompatActivity {
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
 
@@ -37,12 +39,12 @@ public class Feed extends AppCompatActivity {
                 selectedFragment = new FeedFragment();
             } else if (itemId == R.id.nav_habits) {
                 selectedFragment = new HabitsFragment();
+            } else if (itemId == R.id.nav_camera) {
+                selectedFragment = new CameraFragment();
             } else if (itemId == R.id.nav_social) {
                 selectedFragment = new SocialFragment();
-            } else if (itemId == R.id.nav_camera) {
-                selectedFragment = new FeedFragment();
             } else if (itemId == R.id.nav_profile) {
-                selectedFragment = new SocialFragment();
+                selectedFragment = new ProfileFragment();
             }
 
             if (selectedFragment != null) {
