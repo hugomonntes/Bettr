34cba4499f879b01cd24d9a556718ed87f664416
hugomonntes <hmontes.dev@gmail.com>
2026-03-03 20:40:55 +0100

    feat(DesktopApp): Init project from desktop

---
+++ Bettr_Desktop_App/Bettr_Desktop_App/Register.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Bettr_Desktop_App
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Código generado por el Diseñador
+
+        private void InitializeComponent()
+        {
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSubtitle = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.lblBackToLogin = new System.Windows.Forms.Label();
+            this.panelContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.BackColor = System.Drawing.Color.White;
+            this.panelContainer.Controls.Add(this.picLogo);
+            this.panelContainer.Controls.Add(this.lblTitle);
+            this.panelContainer.Controls.Add(this.lblSubtitle);
+            this.panelContainer.Controls.Add(this.lblError);
+            this.panelContainer.Controls.Add(this.lblUsername);
+            this.panelContainer.Controls.Add(this.txtUsername);
+            this.panelContainer.Controls.Add(this.lblEmail);
+            this.panelContainer.Controls.Add(this.txtEmail);
+            this.panelContainer.Controls.Add(this.lblPassword);
+            this.panelContainer.Controls.Add(this.txtPassword);
+            this.panelContainer.Controls.Add(this.lblConfirmPassword);
+            this.panelContainer.Controls.Add(this.txtConfirmPassword);
+            this.panelContainer.Controls.Add(this.btnRegister);
+            this.panelContainer.Controls.Add(this.lblBackToLogin);
+            this.panelContainer.Location = new System.Drawing.Point(250, 40);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(400, 650);
+            this.panelContainer.TabIndex = 0;
+            // 
+            // picLogo
+            // 
+            this.picLogo.Location = new System.Drawing.Point(125, 20);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(150, 100);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 0;
+            this.picLogo.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(116, 132);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(159, 32);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Crear cuenta";
+            // 
+            // lblSubtitle
+            // 
+            this.lblSubtitle.AutoSize = true;
+            this.lblSubtitle.ForeColor = System.Drawing.Color.Gray;
+            this.lblSubtitle.Location = new System.Drawing.Point(129, 173);
+            this.lblSubtitle.Name = "lblSubtitle";
+            this.lblSubtitle.Size = new System.Drawing.Size(130, 13);
+            this.lblSubtitle.TabIndex = 2;
+            this.lblSubtitle.Text = "Regístrate para comenzar";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(60, 200);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 13);
+            this.lblError.TabIndex = 3;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.Location = new System.Drawing.Point(60, 230);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(100, 23);
+            this.lblUsername.TabIndex = 4;
+            this.lblUsername.Text = "Usuario";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(60, 250);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(280, 20);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.Location = new System.Drawing.Point(60, 290);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(100, 23);
+            this.lblEmail.TabIndex = 6;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(60, 310);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(280, 20);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Location = new System.Drawing.Point(60, 350);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(100, 23);
+            this.lblPassword.TabIndex = 8;
+            this.lblPassword.Text = "Contraseña";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(60, 370);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(280, 20);
+            this.txtPassword.TabIndex = 9;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.Location = new System.Drawing.Point(60, 410);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(100, 23);
+            this.lblConfirmPassword.TabIndex = 10;
+            this.lblConfirmPassword.Text = "Confirmar contraseña";
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(60, 430);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(280, 20);
+            this.txtConfirmPassword.TabIndex = 11;
+            this.txtConfirmPassword.UseSystemPasswordChar = true;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(197)))), ((int)(((byte)(94)))));
+            this.btnRegister.FlatAppearance.BorderSize = 0;
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnRegister.ForeColor = System.Drawing.Color.White;
+            this.btnRegister.Location = new System.Drawing.Point(60, 480);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(280, 45);
+            this.btnRegister.TabIndex = 12;
+            this.btnRegister.Text = "Crear cuenta";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            // 
+            // lblBackToLogin
+            // 
+            this.lblBackToLogin.AutoSize = true;
+            this.lblBackToLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblBackToLogin.ForeColor = System.Drawing.Color.Gray;
+            this.lblBackToLogin.Location = new System.Drawing.Point(95, 540);
+            this.lblBackToLogin.Name = "lblBackToLogin";
+            this.lblBackToLogin.Size = new System.Drawing.Size(160, 13);
+            this.lblBackToLogin.TabIndex = 13;
+            this.lblBackToLogin.Text = "¿Ya tienes cuenta? Inicia sesión";
+            // 
+            // Register
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.ClientSize = new System.Drawing.Size(900, 720);
+            this.Controls.Add(this.panelContainer);
+            this.Name = "Register";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bettr - Registro";
+            this.panelContainer.ResumeLayout(false);
+            this.panelContainer.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSubtitle;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label lblBackToLogin;
+    }
+}
